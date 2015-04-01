0b197c79f1abe2fa55f1e48286114fab751c299a
Diego de Souza <diegotrus7@gmail.com>
2015-04-01 15:48:45 -0400

    created all the winning combinations

--- js/tic_tac_toe.js
+++ js/tic_tac_toe.js
@@ -1,3 +1,19 @@
+var winningCombinations = [
+  // Horizontals
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+
+  // Verticals
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+
+  // Diagonals
+  [0, 4, 8],
+  [2, 4, 6]
+];
+
 // Initialize current player as player 'x'
 var currentPlayerToken = 'x';
 
