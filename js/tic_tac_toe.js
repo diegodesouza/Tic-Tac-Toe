9c44d01d31cc4f15fc350bad433e640d9166bf69
Diego de Souza <diegotrus7@gmail.com>
2015-04-01 15:41:28 -0400

    added multiplayer

--- js/tic_tac_toe.js
+++ js/tic_tac_toe.js
@@ -1,5 +1,15 @@
-$('.board').on('click', '.square', function(){
+// Initialize current player as player 'x'
+var currentPlayerToken = 'x';
+
+$('.board').on('click', '.square', function(event) {
   // Select the square
   var $square = $(event.currentTarget);
-  $square.addClass('square-x');
+  $square.addClass('square-' + currentPlayerToken);
+
+  // Swap current player's token
+  if (currentPlayerToken === 'x') {
+    currentPlayerToken = 'o';
+  } else {
+    currentPlayerToken = 'x';
+  }
 });
